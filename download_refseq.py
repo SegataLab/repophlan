091346e651f5d586b6193414e76fe552037b0a27
Roman Stolyarov <romka@mit.edu>
2013-07-01 21:47:13 +0200

    Automatically downloads all complete*genomic.fna.gz files.

--- download_refseq.py
+++ download_refseq.py
@@ -1,9 +1,26 @@
 import urllib
 import subprocess
+from ftplib import FTP
 
-urllib.urlretrieve('ftp://ftp.ncbi.nlm.nih.gov/refseq/release/complete/complete*.genomic.fna.gz')
-urllib.urlretrieve('ftp://ftp.ncbi.nlm.nih.gov/refseq/release/complete/complete*.genomic.gbff.gz')
-#urllib.urlretrieve('ftp://ftp.ncbi.nlm.nih.gov/refseq/release/complete/complete*.protein.gpff.gz')
-bashCommand = "gunzip *.fna.gz"
+ftp = FTP('ftp.ncbi.nlm.nih.gov')
+ftp.login()
+ftp.cwd('refseq/release/complete/')
+all_files = []
+ftp.dir(all_files.append)
+print 'Got list of files'
+for file in all_files:
+  fn = file.split()[-1]
+  if fn[:8] == 'complete' and fn[-14:] == 'genomic.fna.gz':
+    print 'Downloading',fn,'....'
+    urllib.urlretrieve('ftp://ftp.ncbi.nlm.nih.gov/refseq/release/complete/'+fn,'/banche_dati/sharedCM/genomicDB/'+fn)
+    print 'Downloaded',fn
+
+
+
+
+
+
+
+bashCommand = "gunzip /banche_dati/sharedCM/genomicDB/*.gz"
 process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
 output = process.communicate()[0]  
