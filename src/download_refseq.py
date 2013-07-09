2ebdc21608c79dc60c2afca3fdf697e923a10271
Roman Stolyarov <romka@mit.edu>
2013-07-09 21:24:49 +0200

    Done with download_refseq. Uses timestamping; input settings file; extracts *.fna.gz

--- src/download_refseq.py
+++ src/download_refseq.py
@@ -1,26 +1,42 @@
 import urllib
 import subprocess
 from ftplib import FTP
+import csv
+import os
 
-ftp = FTP('ftp.ncbi.nlm.nih.gov')
+def readSettings():
+        settings = []
+        with open("settings.txt") as tsv:
+                for line in csv.reader(tsv,dialect="excel-tab"):
+                        settings.append(line)
+        return settings
+
+#Parse settings file, set relevant variables
+settings = readSettings()
+refseqdbftp = settings[9][1]
+refseqdbpath = settings[10][1]
+refseqdbloc = settings[11][1]
+refseqdburl = 'ftp://'+refseqdbftp+'/'+refseqdbpath
+
+#Access the FTP, download files with names ending in genomic.fna.gz or genomic.gbff.gz
+ftp = FTP(refseqdbftp)
 ftp.login()
-ftp.cwd('refseq/release/complete/')
+ftp.cwd(refseqdbpath)
 all_files = []
 ftp.dir(all_files.append)
 print 'Got list of files'
 for file in all_files:
-  fn = file.split()[-1]
-  if fn[:8] == 'complete' and fn[-14:] == 'genomic.fna.gz':
-    print 'Downloading',fn,'....'
-    urllib.urlretrieve('ftp://ftp.ncbi.nlm.nih.gov/refseq/release/complete/'+fn,'/banche_dati/sharedCM/genomicDB/'+fn)
-    print 'Downloaded',fn
+	fn = file.split()[-1]
+	if fn[:12] == 'complete.100' and (fn[-14:] == 'genomic.fna.gz' or fn[-15:] == 'genomic.gbff.gz'):
+		print 'Downloading',fn,'....'
+		try:
+  			bashCommand = 'wget -N -P '+refseqdbloc+' '+refseqdburl+fn
+          		process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+           		output = process.communicate()[0]
+           		print "Downloaded "+fn
+    		except:
+           		print "Error downloading "+fn
 
-
-
-
-
-
-
-bashCommand = "gunzip /banche_dati/sharedCM/genomicDB/*.gz"
-process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-output = process.communicate()[0]  
+#Uncompress *.fna.gz, but keep the compressed files to allow timestamping of future downloads
+bashCommand = 'for FILE in '+refseqdbloc+'*.fna.gz; do gunzip -c $FILE > ${FILE%.*}; done'
+os.system(bashCommand)
