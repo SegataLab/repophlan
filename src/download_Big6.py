245a41d9a8b83d641f585a7a98fd447b2ce54e19
rstolyarov <rstolyarov@localhost>
2013-07-09 18:58:56 +0200

    Added timestamping

--- src/download_Big6.py
+++ src/download_Big6.py
@@ -4,7 +4,7 @@ import os
 import subprocess
 import argparse
 import csv
-
+import time
 
 def parseArgs():
 	parser = argparse.ArgumentParser()
@@ -13,9 +13,10 @@ def parseArgs():
 	parser.add_argument("--prok_file", help="Prokaryotes file name", default="prokaryotes.txt")
 	parser.add_argument("--euk_file", help="Eukaryotes file name", default="eukaryotes.txt")
 	parser.add_argument("--vir_file", help="Viruses file name", default="viruses.txt")
-	parser.add_argument("--ncbi_nodes", help="NCBI nodes file name", default="nodes.txt")
-	parser.add_argument("--ncbi_names", help="NCBI names file name", default="names.txt")
-	parser.add_argument("--logfile", help="Log file name", default='log.txt')
+	parser.add_argument("--ncbi_nodes", help="NCBI nodes file name", default="nodes.dmp")
+	parser.add_argument("--ncbi_names", help="NCBI names file name", default="names.dmp")
+	parser.add_argument("--logfile", help="Log file name", default="log.txt")
+	parser.add_argument("--outputdir", help="Output directory name", default="Big6")
 	args = parser.parse_args()
 	return args
 
@@ -30,14 +31,22 @@ def readSettings():
 def makeNewDirectory(newpath): 
 	if not os.path.exists(newpath): 
   		os.makedirs(newpath)
+	os.chdir(newpath)
+
+def download(url,logfile):
+	try:
+        	os.system('wget -q -N '+url)
+        	f.write("Downloaded file from "+url+"\n")
+	except:
+        	f.write("Error downloading file from "+url+"\n")
 
 args = parseArgs()
-makeNewDirectory(r'Big6')
 settings = readSettings()
-f = open('Big6/'+args.logfile,'w')
-f.write('Log file for download_Big6.py:\n')
+makeNewDirectory(args.outputdir)
+f = open(args.logfile,'a')
+f.write(time.strftime("\nRun of download_Big6 on %Y-%m-%d %H:%M:%S\n", time.gmtime()))
 
-#Download all files
+#Initialize settings variables
 taxdumpurl = settings[0][1]
 taxdump = settings[1][1]
 refsequrl = settings[2][1]
@@ -48,48 +57,38 @@ euk_file = settings[6][1]
 vir_file = settings[7][1]
 delfiles = settings[8][1:]
 
-try:
-	os.system('wget -P Big6/'+taxdump+' '+taxdumpurl+taxdump)
-	f.write("Downloaded",taxdump)
-except:
-	f.write("Error downloading "+taxdump)
+download(taxdumpurl+taxdump,args.logfile)
 
 refseq = rf+args.rsversion+'.catalog'
 refseqgz = refseq+'.gz'
+
 if not os.path.isfile(refseq):
-  try:
-	os.system('wget -P Big6/'+refseqgz+' '+refsequrl+refseqgz)
-    	f.write("Downloaded latest version of RefSeq catalog")
-  except:
-    	f.write("Error downloading specified version of RefSeq catalog. Please ensure you specify latest RefSeq catalog version.")
+	download(refsequrl+refseqgz,args.logfile)
 else:
-	f.write("Latest version of RefSeq catalog already on file")
+	f.write("Latest version of RefSeq catalog already on file\n")
 
 tfs = [[prok_file,args.prok_file],[euk_file,args.euk_file], [vir_file,args.vir_file]]
 for tf in tfs:
-	try:
-		os.system('wget -P Big6/'+tf[1]+' '+textfilesurl+tf[0])
-  		f.write("Downloaded "+tf[0]+" and saved to local file "+tf[1])
-	except:
-		f.write("Error downloading "+tf[0])
+	download(textfilesurl+tf[0],args.logfile)
+	os.rename(tf[0],tf[1])
   
 #Untar/gunzip
-os.chmod('Big6')
 tfile = tarfile.open(taxdump)
 if tarfile.is_tarfile(taxdump):
   # extract all contents
   tfile.extractall('.')
 else:
-  f.write(taxdump + " is not a tarfile.")
-    
-bashCommand = "gunzip "+refseqgz
-process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
-output = process.communicate()[0]    
-f.write("Unzipped refseq catalog")    
+  f.write(taxdump + " is not a tarfile.\n")
+
+if os.path.isfile(refseqgz):    
+	bashCommand = "gunzip "+refseqgz
+	process = subprocess.Popen(bashCommand.split(), stdout=subprocess.PIPE)
+	output = process.communicate()[0]    
+	f.write("Unzipped refseq catalog\n")    
 
 for df in delfiles:
   os.remove(df)
 os.rename('names.dmp',args.ncbi_names)
 os.rename('nodes.dmp',args.ncbi_nodes)
 
-f.write("Deleted extra files")
+f.write("Deleted extra files\n")
