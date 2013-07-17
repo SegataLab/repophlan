89509be96748e243c930ccb115c65bd1a9c38001
Roman Stolyarov <r.m.stolyarov@gmail.com>
2013-07-17 19:46:17 +0000

    Added some comments

--- src/scafs.py
+++ src/scafs.py
@@ -16,10 +16,15 @@ def read_params(args):
 
 def get_scaffolds( inp ):
     scfs = []
+    #Go through lines of master record
     for l in gzip.open( inp ):
+        #Get scaffold master accession number
         if l.startswith( "ACCESSION" ):
             acc = l.strip().split()[1]
+        #Get individual contig accession numbers
         elif l.startswith( "WGS_SCAFLD" ): 
+            #There will either be a single accession or a range. 
+            #If range, get all accessions in the range. Append to scfs.
             scf = l.strip().split()[1].split("-")
             if len(scf) == 1:
                 scfs.append( scf[0] )
@@ -32,12 +37,17 @@ def get_scaffolds( inp ):
 
 def run(pars):
     scafs = []
+    
+    #For all draft genome master records, if the annotation file 
+    #is not available, get all contig accessions for the scaffold manually.
     for mstr in glob.glob(pars['refseq_dir']+"/completeNZ_*.mstr.gbff.gz" ):
         fna_in = pars['refseq_dir']+".".join(os.path.basename( mstr ).split(".")[:1]) + ".genomic.gbff.gz" 
         if not os.path.exists( fna_in ):
             scaf = get_scaffolds( mstr )
             if scaf:
                 scafs.append(scaf)
+                
+    #Output all scaffolds as separate lines to file.
     with open( pars['output'], "w" ) as out:
         for f,t in scafs:
             out.write( f+"\t"+t+"\n" )
